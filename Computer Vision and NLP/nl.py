d8e9d2cd04ef90b4aecd036f193cb62c1cafc17d
LleilaA13 <zanonileila3@gmail.com>
2024-04-17 09:25:47 +0200

    notes

---
+++ Computer Vision and NLP/nl.py
@@ -0,0 +1,10 @@
+import nltk
+
+nltk.download('omw-1.4')
+nltk.download('tagsets')
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
+
+nltk.download

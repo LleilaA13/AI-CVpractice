c4ff4241b954896caaef17c09f5764ba61f12c08
LleilaA13 <zanonileila3@gmail.com>
2024-04-24 09:11:42 +0200

    notes

--- Computer Vision and NLP/lectapr19th.py
+++ Computer Vision and NLP/lectapr19th.py
@@ -1,8 +1,24 @@
 import seaborn as sns
-
-
+import matplotlib.pylab as plt
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 iris = sns.load_dataset('iris')
-print(iris.head())
+#print(iris.head())
 
 #our_table['Name']
 
+#sns.pairplot(iris, hue = 'species')
+#plt.show()
+
+X_iris = iris.drop('species', axis = 1) #remove column  -> (axis = 1)
+#print(X_iris.shape)
+y_iris = iris['species']
+#print(y_iris.shape)
+
+model = GaussianNB()
+model.fit(X_iris, y_iris)
+
+y_pred = model.predict(X_iris)
+accuracy = accuracy_score(y_pred, y_iris)
+
+print(accuracy)
--- ML/ML/GMM.md
+++ ML/ML/GMM.md
@@ -1,4 +1,5 @@
 P(X) as a linear combination
 convex com of gaussian,
  pie all non neg, sum up to 1
- 
+
+
